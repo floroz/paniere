6d16d7feb641d622e877e2f6caefcb376b039fc1
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2025-05-03 17:10:35 +0200

    feat: creat i18n zustand store for languages

---
+++ src/store/useLanguageStore.ts
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const LOCAL_STORAGE_KEY = "paniere-language";
+
+export type Language = "en" | "it";
+
+interface LanguageState {
+  language: Language;
+}
+
+interface LanguageStateWithActions extends LanguageState {
+  setLanguage: (language: Language) => void;
+}
+
+const initialState: LanguageState = {
+  language: "en",
+};
+
+/**
+ * Store for managing language preferences
+ */
+export const useLanguageStore = create<LanguageStateWithActions>()(
+  persist(
+    (set) => ({
+      ...initialState,
+
+      setLanguage: (language: Language) => {
+        set({ language });
+      },
+    }),
+    {
+      name: LOCAL_STORAGE_KEY,
+    }
+  )
+);
